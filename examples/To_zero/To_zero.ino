3d1f37f5a894c03a62af4f91258d3ce3324f9902
Egor-droid <egorkotar@yandex.ru>
2020-12-31 10:51:14 +0700

    Create To_zero.ino

---
+++ examples/To_zero/To_zero.ino
@@ -0,0 +1,13 @@
+#include <step.h>
+
+void setup() {
+  step mot(0, 1, 2, 3, 4, 4096*16/360);
+  mot.move(10);
+  mot.move();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
