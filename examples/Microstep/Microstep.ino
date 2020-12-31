67379f731492cd3db76ef6dd97bd0dde00c76faf
Egor-droid <egorkotar@yandex.ru>
2020-12-31 10:47:33 +0700

    Create Microstep.ino

---
+++ examples/Microstep/Microstep.ino
@@ -0,0 +1,12 @@
+#include <step.h>
+
+void setup() {
+  step mot(0, 1, 2, 3, 4, 4096*16/360);
+  mot.move(10, 0, 128, 8);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
