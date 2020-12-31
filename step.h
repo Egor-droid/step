e0215b5c03a0d1129def1d0b525ac87814dd1310
Egor-droid <egorkotar@yandex.ru>
2020-12-31 10:26:28 +0700

    Update step.h

--- step.h
+++ step.h
@@ -7,27 +7,29 @@ class step{
         byte ms1;
         byte ms2;
         byte ms3;
+        long cof;
     public:
-        long pos=0;
-        step(byte step, byte dir, byte ms1, byte ms2, byte ms3){
+        float pos=0;
+        step(byte step, byte dir, byte ms1, byte ms2, byte ms3, long cof){
             step=step;
             dir=dir;
             ms1=ms1;
             ms2=ms2;
             ms3=ms3;
+            cof=cof;
             pinMode(step, OUTPUT);
             pinMode(dir, OUTPUT);
             pinMode(ms1, OUTPUT);
             pinMode(ms2, OUTPUT);
             pinMode(ms3, OUTPUT);
         }
-        void move(long cord=0, bool rel=0, byte speed=128, byte ms=16){
+        void move(float cord=0, bool rel=0, byte speed=128, byte ms=16){
             cord+=pos*rel;
             digitalWrite(dir, pos>cord);
             digitalWrite(ms1, ms==2||ms>4);
             digitalWrite(ms2, ms>2);
             digitalWrite(ms3, ms==16);
-            for(pos=pos; pos!=cord; pos+=copysign(16, cord-pos)/ms){
+            for(pos=pos; pos!=cord; pos+=copysign(1, cord-pos)/cof*16/ms){
                 digitalWrite(step, 1);
                 delay(256-speed);
                 digitalWrite(step, 0);
